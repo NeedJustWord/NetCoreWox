ea280789c426b93f8c65bf6171b388977206a396
NeedJustWord <13718169+NeedJustWord@users.noreply.github.com>
2022-05-04 23:18:05 +0800

    Wox.Core

---
+++ Wox.Core/Resource/InternationalizationManager.cs
@@ -0,0 +1,26 @@
+﻿namespace Wox.Core.Resource
+{
+    public static class InternationalizationManager
+    {
+        private static Internationalization instance;
+        private static readonly object syncObject = new object();
+
+        public static Internationalization Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncObject)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Internationalization();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+    }
+}
---
+++ Wox.Core/Resource/LocalizationConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+using System.Windows.Data;
+
+namespace Wox.Core
+{
+    public class LocalizationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType == typeof(string) && value != null)
+            {
+                FieldInfo fi = value.GetType().GetField(value.ToString());
+                if (fi != null)
+                {
+                    string localizedDescription = string.Empty;
+                    var attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+                    if ((attributes.Length > 0) && (!string.IsNullOrEmpty(attributes[0].Description)))
+                    {
+                        localizedDescription = attributes[0].Description;
+                    }
+
+                    return string.IsNullOrEmpty(localizedDescription) ? value.ToString() : localizedDescription;
+                }
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
